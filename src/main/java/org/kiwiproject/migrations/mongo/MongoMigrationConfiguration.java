27fc324ba5a4fefef2e0b6a8daaa3dd3e649f038
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2021-11-23 18:03:21 -0500

    Add ability to change the default `db` command name
    Closes #21

--- src/main/java/org/kiwiproject/migrations/mongo/MongoMigrationConfiguration.java
+++ src/main/java/org/kiwiproject/migrations/mongo/MongoMigrationConfiguration.java
@@ -1,11 +1,18 @@
 package org.kiwiproject.migrations.mongo;
 
 public interface MongoMigrationConfiguration<T> {
+
+    String DEFAULT_NAME = "db";
+
     String getMigrationPackage(T config);
     String getMongoUri(T config);
     String getDatabaseName(T config);
 
     default boolean shouldDisableTransactions(T config) {
         return false;
+    }
+
+    default String getCommandName() {
+        return DEFAULT_NAME;
     }
 }
