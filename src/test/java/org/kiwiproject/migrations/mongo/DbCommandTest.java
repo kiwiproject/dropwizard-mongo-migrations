85eb035083263e6aaf351bae7e58b31dcd366fbc
chrisrohr <51920+chrisrohr@users.noreply.github.com>
2021-11-23 10:05:27 -0500

    Forgot the bit that actually adds the subcommands to the parser.
    Add a few more tests for full coverage.

--- src/test/java/org/kiwiproject/migrations/mongo/DbCommandTest.java
+++ src/test/java/org/kiwiproject/migrations/mongo/DbCommandTest.java
@@ -1,14 +1,20 @@
 package org.kiwiproject.migrations.mongo;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.kiwiproject.test.junit.jupiter.MongoServerExtension;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.Map;
 
 class DbCommandTest {
@@ -31,6 +37,38 @@ class DbCommandTest {
         var db = client.getDatabase(MONGO_SERVER_EXTENSION.getTestDatabaseName());
 
         assertThat(db.getCollection("myCollection").countDocuments()).isEqualTo(1);
+    }
+
+    @Test
+    void testPrintHelp() throws Exception {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        createSubparser(dbCommand).printHelp(new PrintWriter(new OutputStreamWriter(baos, UTF_8), true));
+        assertThat(baos.toString(UTF_8.name())).isEqualTo(String.format(
+                "usage: db db [-h] {migrate} ...%n" +
+                        "%n" +
+                        "Run database migration tasks%n" +
+                        "%n" +
+                        "positional arguments:%n" +
+                        "  {migrate}%n" +
+                        "%n" +
+                        "named arguments:%n" +
+                        "  -h, --help             show this help message and exit%n"));
+    }
+
+    protected static Subparser createSubparser(AbstractMongockCommand<?> command) {
+        final Subparser subparser = ArgumentParsers.newFor("db")
+                .terminalWidthDetection(false)
+                .build()
+                .addSubparsers()
+                .addParser(command.getName())
+                .description(command.getDescription());
+        command.configure(subparser);
+        return subparser;
+    }
+
+    @Test
+    void shouldReturnConfigurationClass() {
+        assertThat(dbCommand.getConfigurationClass()).isEqualTo(TestMigrationConfiguration.class);
     }
 }
 
