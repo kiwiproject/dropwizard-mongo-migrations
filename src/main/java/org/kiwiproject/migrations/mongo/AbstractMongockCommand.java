b2b0f1291621ac7079cac2d0d48107ab5f6ff644
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-04-21 21:43:56 +0000

    Validate driver and migration package
    * Validate the ConnectionDriver and migration package in
      AbstractMongockCommand#run
    
    Misc:
    
    * Add a logback configuration in test resources
    * Add a .keep in src/main/resources to preserve the expected Maven
      directory structure
    
    Closes #69

--- src/main/java/org/kiwiproject/migrations/mongo/AbstractMongockCommand.java
+++ src/main/java/org/kiwiproject/migrations/mongo/AbstractMongockCommand.java
@@ -1,12 +1,18 @@
 package org.kiwiproject.migrations.mongo;
 
+import static com.google.common.base.Preconditions.checkState;
+import static java.util.Objects.nonNull;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 import io.dropwizard.Configuration;
 import io.dropwizard.cli.ConfiguredCommand;
 import io.dropwizard.setup.Bootstrap;
 import io.mongock.runner.core.executor.MongockRunner;
 import io.mongock.runner.standalone.MongockStandalone;
+import lombok.extern.slf4j.Slf4j;
 import net.sourceforge.argparse4j.inf.Namespace;
 
+@Slf4j
 public abstract class AbstractMongockCommand<T extends Configuration> extends ConfiguredCommand<T> {
     private final Class<T> configurationClass;
     private final MongoMigrationConfiguration<T> migrationConfiguration;
@@ -25,9 +31,16 @@ public abstract class AbstractMongockCommand<T extends Configuration> extends Co
     @Override
     protected void run(Bootstrap<T> bootstrap, Namespace namespace, T configuration) {
         var driver = migrationConfiguration.getConnectionDriver(configuration);
+        checkState(nonNull(driver), "connectionDriver must not be null");
+
+        var migrationPackage = migrationConfiguration.getMigrationPackage(configuration);
+        checkState(isNotBlank(migrationPackage), "migrationPackage must not be blank");
+
+        LOG.info("Using driver {} and migrationPackage {}", driver.getClass(), migrationPackage);
+
         var runner = MongockStandalone.builder()
                 .setDriver(driver)
-                .addMigrationScanPackage(migrationConfiguration.getMigrationPackage(configuration))
+                .addMigrationScanPackage(migrationPackage)
                 .buildRunner();
 
         run(namespace, runner);
